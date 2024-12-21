545f959729cacfc28a6fd9b779adcedf8272d70f
Naneet <157294737+Naneet@users.noreply.github.com>
2024-12-21 18:39:51 +0530

    Update pickle.py

--- utils/pickle.py
+++ utils/pickle.py
@@ -12,3 +12,10 @@ def pickle_data(path, dataloader):
 
 def read_pickle(path) -> torch.tensor:
     return torch.load(path)
+
+def pickle_files_path_list(path) -> list:
+    path_list = []
+    for top, dirs, files in os.walk(path):
+        for nm in files:       
+            path_list.append(os.path.join(top, nm))
+    return path_list
