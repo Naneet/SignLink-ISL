bf799f3aa8f03ab27aaa44f083d716c4f50c2362
BARKKNIGHT <yatharthk2406@gmail.com>
2024-12-21 18:40:27 +0530

    changed trainer.py to accept pickled data tensors

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -1,6 +1,7 @@
 import torch
 from utils.save_load import save_checkpoint
 from torch.amp import autocast, GradScaler
+from utils.pickle import read_pickle
 
 class Trainer:
     def __init__(self,model,device,optimizer,loss_fn,train_dataloader,test_dataloader,save=False):
@@ -12,6 +13,7 @@ class Trainer:
         self.test_dataloader = test_dataloader
         self.model = model
         self.save = save
+
     def train_step(self,epoch):
         self.model.train()
         train_loss, acc = 0, 0
@@ -70,6 +72,7 @@ class Trainer:
             acc = total_correct * 100 / total_samples
             test_loss = test_loss / len(self.test_dataloader)
             print(f"Epoch: {epoch} | Test Loss: {test_loss:.4f} | Accuracy: {acc:.2f}")
+            print("************************")
             
             if self.save:
                 print(f"Saving model at epoch {epoch}...")
@@ -79,4 +82,84 @@ class Trainer:
                     'optimizer_state_dict': self.optimizer.state_dict(),
                     'best_metric': acc
                 }, filename=f"resnet18_pretrained_acc={acc:.2f}_{epoch=}_real.pth")
+    
+    def train_step_pickle(self,epoch,path_list):
+        self.model.train()
+        train_loss, acc = 0, 0
+        total_correct = 0
+        total_samples = 0
+        
+
+        for path in path_list:
+            tensor = read_pickle(path=path)
+            X = tensor['video'].to(self.device)
+            y = tensor['label'].to(self.device)
+
+            # Automatic Mixed Precision (AMP)
+            with autocast('cuda'):
+                y_pred = self.model(X)
+                loss = self.loss_fn(y_pred, y)
+                train_loss += loss.item()
+
+            # Scale the loss and call backward
+            self.scaler.scale(loss).backward()
+            # Update parameters with scaled gradients
+            self.scaler.step(self.optimizer)
+            # Update scaler for next iteration
+            self.scaler.update()
+
+            self.optimizer.zero_grad()
+
+            # Compute predictions and accuracy
+            y_pred_class = torch.argmax(torch.softmax(y_pred, dim=1), dim=1)
+            total_correct += (y_pred_class == y).sum().item()
+            total_samples += y.size(0)
+
+            del X
+            del y
+
+        acc = total_correct * 100 / total_samples
+        train_loss = train_loss / len(self.train_dataloader)
+        print(f"Epoch: {epoch} | Train Loss: {train_loss:.4f} | Accuracy: {acc:.2f}")
+
+
+    def test_step_pickle(self, epoch,path_list):
+        self.model.eval()
+        test_loss, acc = 0, 0
+        total_correct = 0
+        total_samples = 0
+
+        with torch.no_grad():
+            for path in path_list:
+                tensor = read_pickle(path=path)
+                X = tensor['video'].to(self.device)
+                y = tensor['label'].to(self.device)
+
+                # Use AMP for inference
+                with autocast('cuda'):
+                    y_pred = self.model(X)
+                    loss = self.loss_fn(y_pred, y)
+                    test_loss += loss.item()
+
+                # Compute predictions and accuracy
+                y_pred_class = torch.argmax(torch.softmax(y_pred, dim=1), dim=1)
+                total_correct += (y_pred_class == y).sum().item()
+                total_samples += y.size(0)
+
+            acc = total_correct * 100 / total_samples
+            test_loss = test_loss / len(self.test_dataloader)
+            print(f"Epoch: {epoch} | Test Loss: {test_loss:.4f} | Accuracy: {acc:.2f}")
             print("************************")
+            
+            if self.save:
+                print(f"Saving model at epoch {epoch}...")
+                save_checkpoint({
+                    'epoch': epoch,
+                    'model_state_dict': self.model.state_dict(),
+                    'optimizer_state_dict': self.optimizer.state_dict(),
+                    'best_metric': acc
+                }, filename=f"resnet18_pretrained_acc={acc:.2f}_{epoch=}_real.pth")
+        
+    
+        
+    
