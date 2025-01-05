331be337a4da464e01df57cf6b40d68f4182a059
Naneet <157294737+Naneet@users.noreply.github.com>
2025-01-05 10:44:50 +0530

    Update save_load.py

--- utils/save_load.py
+++ utils/save_load.py
@@ -8,4 +8,7 @@ def load_checkpoint(filename="checkpoint.pth"):
     """Load model and optimizer state from a file."""
     checkpoint = torch.load(filename)
     return checkpoint
-        
+
+def load_model(model, checkpoint):
+    model.load_state_dict(checkpoint['model_state_dict'])
+    return model
