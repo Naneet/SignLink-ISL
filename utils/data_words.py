b5eb19a4f7f3f46d10efb7d4f91d6a4284604575
Naneet <157294737+Naneet@users.noreply.github.com>
2025-01-05 12:16:58 +0530

    Update data_words.py

--- utils/data_words.py
+++ utils/data_words.py
@@ -18,3 +18,25 @@ def data_and_words(path):
     return data, words_list, words_dict  # data will return a list of tuples where data[0] is the path to the video and data[1] is the label
                                          # words_list can be used to convert index to word
                                          # words_dict can be used to convert word to index
+
+
+def random_split(data_dict, train_ratio=0.8, seed=42):
+    np.random.seed(seed)
+    train_data = []
+    test_data = []
+
+    for label, paths in data_dict.items():
+        # Shuffle the paths
+        paths = np.array(paths)
+        np.random.shuffle(paths)
+
+        # Split into training and testing
+        split_idx = int(len(paths) * train_ratio)
+        train_paths = paths[:split_idx].tolist()
+        test_paths = paths[split_idx:].tolist()
+
+        # Append the paths with their labels
+        train_data.extend([(path, word_to_idx[label]) for path in train_paths])
+        test_data.extend([(path, word_to_idx[label]) for path in test_paths])
+
+    return train_data, test_data
